71636492394350739ba03597a7fcd8fcebe46fdf
einfachesache <klugetommy@web.de>
2025-09-23 01:51:25 +0200

    Changes -> prevent disenchanting of custom items

--- src/main/java/de/einfachesache/flareonevents/listener/EnchantItemListener.java
+++ src/main/java/de/einfachesache/flareonevents/listener/EnchantItemListener.java
@@ -4,6 +4,8 @@ import de.einfachesache.flareonevents.util.ItemUtils;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.EnchantItemEvent;
+import org.bukkit.event.inventory.PrepareGrindstoneEvent;
+import org.bukkit.inventory.GrindstoneInventory;
 
 public class EnchantItemListener implements Listener {
 
@@ -11,6 +13,14 @@ public class EnchantItemListener implements Listener {
     public void onEnchant(EnchantItemEvent e) {
         if (ItemUtils.isInvulnerable(e.getItem())) {
             e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onPrepareGrindstone(PrepareGrindstoneEvent event) {
+        GrindstoneInventory inv = event.getInventory();
+        if (ItemUtils.isInvulnerable(inv.getUpperItem()) || ItemUtils.isInvulnerable(inv.getLowerItem())) {
+            event.setResult(null);
         }
     }
 }
