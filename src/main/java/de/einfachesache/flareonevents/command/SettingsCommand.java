be4832404c5473a230734d4a6bd53e75235b9167
einfachesache <klugetommy@web.de>
2025-10-02 18:24:49 +0200

    Changes -> reworked SettingsCommand

--- src/main/java/de/einfachesache/flareonevents/command/SettingsCommand.java
+++ src/main/java/de/einfachesache/flareonevents/command/SettingsCommand.java
@@ -11,13 +11,12 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class SettingsCommand implements CommandExecutor, TabCompleter {
 
-    private static final List<String> SUB_COMMANDS = Arrays.asList("PVP", "NETHER", "VIEW_DISTANZ", "SIMULATION_DISTANZ");
+    private static final List<String> SUB_COMMANDS = Arrays.asList("PVP", "NETHER", "VIEW_DISTANCE", "SIMULATION_DISTANCE");
 
     FlareonEvents plugin = FlareonEvents.getPlugin();
 
@@ -31,10 +30,10 @@ public class SettingsCommand implements CommandExecutor, TabCompleter {
 
         String sub = args[0].toUpperCase();
         return switch (sub) {
-            case "PVP" -> handlePvP(sender);
-            case "NETHER" -> handleNether(sender);
-            case "VIEW_DISTANZ" -> handleViewDistanz(sender, args);
-            case "SIMULATION_DISTANZ" -> handleSimulationDistanz(sender, args);
+            case "PVP" -> handlePvP(sender, args);
+            case "NETHER" -> handleNether(sender, args);
+            case "VIEW_DISTANCE" -> handleViewDistanz(sender, args);
+            case "SIMULATION_DISTANCE" -> handleSimulationDistanz(sender, args);
             default -> {
                 sendUsage(sender, label);
                 yield false;
@@ -42,61 +41,101 @@ public class SettingsCommand implements CommandExecutor, TabCompleter {
         };
     }
 
-    private boolean handlePvP(CommandSender sender) {
+    private boolean handlePvP(CommandSender sender, String[] args) {
         boolean pvp = Bukkit.getWorlds().getFirst().getPVP();
-        plugin.getServer().getWorlds().forEach(world -> world.setPVP(!pvp));
-        sender.sendMessage("§cPVP wurde erfolgreich " + (!pvp ? "§aaktiviert!" : "§cdeaktiviert!"));
+
+        if (args.length != 2) {
+            sender.sendMessage("§aDas §aPVP ist aktuell " + (pvp ? "§aaktiviert!" : "§cdeaktiviert!"));
+        } else {
+            plugin.getServer().getWorlds().forEach(world -> world.setPVP(!pvp));
+            sender.sendMessage("§aDas §ePVP §awurde erfolgreich " + (!pvp ? "§aaktiviert!" : "§cdeaktiviert!"));
+        }
+
         return true;
     }
 
-    private boolean handleNether(CommandSender sender) {
+    private boolean handleNether(CommandSender sender, String[] args) {
         boolean netherEnabled = PortalCreateListener.netherEnabled();
-        PortalCreateListener.setNether(!netherEnabled);
-        sender.sendMessage("§cDer Nether wurde erfolgreich " + (!netherEnabled ? "§aaktiviert!" : "§cdeaktiviert!"));
+
+        if (args.length != 2) {
+            sender.sendMessage("§aDer §cNether §aist aktuell " + (netherEnabled ? "§aaktiviert!" : "§cdeaktiviert!"));
+        } else {
+            PortalCreateListener.setNether(!netherEnabled);
+            sender.sendMessage("§aDer §cNether §awurde erfolgreich " + (!netherEnabled ? "§aaktiviert!" : "§cdeaktiviert!"));
+        }
         return true;
     }
 
     private boolean handleViewDistanz(CommandSender sender, String[] args) {
-        int distanz;
+
+        if (args.length != 2) {
+            sender.sendMessage("§aDie aktuelle §eView Distance §aist bei §e" + Bukkit.getWorlds().getFirst().getSendViewDistance() + "§a Chucks.");
+            return true;
+        }
+
+        int distance;
         try {
-            distanz = Integer.parseInt(args[1]);
-            Bukkit.getWorlds().forEach(world -> world.setViewDistance(distanz));
+            distance = Integer.parseInt(args[1]);
+            Bukkit.getWorlds().forEach(world -> world.setViewDistance(distance));
         } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
-            sender.sendMessage("§cBitte gebe eine gültige Distanz an /setting VIEW_DISTANZ 2-32");
+            sender.sendMessage("§cBitte gebe eine gültige Distance an /setting VIEW_DISTANCE 2-32");
             return false;
         }
 
-        sender.sendMessage("§aDie View Distanz wurde erfolgreich auf §e" + distanz + " §agesetzt");
+        sender.sendMessage("§aDie §eView Distance §awurde erfolgreich auf §e" + distance + " §agesetzt");
         return true;
     }
 
     private boolean handleSimulationDistanz(CommandSender sender, String[] args) {
-        int distanz;
+
+        if (args.length != 2) {
+            sender.sendMessage("§aDie aktuelle §eSimulation Distance §aist bei §e" + Bukkit.getWorlds().getFirst().getSimulationDistance() + "§a Chucks.");
+            return true;
+        }
+
+        int distance;
         try {
-            distanz = Integer.parseInt(args[1]);
-            Bukkit.getWorlds().forEach(world -> world.setSimulationDistance(distanz));
+            distance = Integer.parseInt(args[1]);
+            Bukkit.getWorlds().forEach(world -> world.setSimulationDistance(distance));
         } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
-            sender.sendMessage("§cBitte gebe eine gültige Distanz an /setting SIMULATION_DISTANZ 2-32");
+            sender.sendMessage("§cBitte gebe eine gültige Distance an /setting SIMULATION_DISTANCE 2-32");
             return false;
         }
 
-        sender.sendMessage("§aDie Simulation Distanz wurde erfolgreich auf §e" + distanz + " §agesetzt");
+        sender.sendMessage("§aDie §eSimulation Distance §awurde erfolgreich auf §e" + distance + " §agesetzt");
         return true;
     }
 
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, String[] args) {
+
         if (args.length == 1) {
-            String input = args[0].toUpperCase();
-            List<String> completions = new ArrayList<>();
-            for (String sub : SUB_COMMANDS) {
-                if (sub.startsWith(input)) {
-                    completions.add(sub);
-                }
-            }
-            return completions;
+            String input = args[0].toUpperCase(Locale.ROOT);
+            return SUB_COMMANDS.stream()
+                    .filter(sub -> sub.startsWith(input))
+                    .sorted()
+                    .toList();
         }
-        return new ArrayList<>();
+
+        if (args.length == 2) {
+            String firstInputUpperCase = args[0].toUpperCase(Locale.ROOT);
+            String secondInput = args[1];
+
+            if (firstInputUpperCase.equals("PVP") || firstInputUpperCase.equals("NETHER")) {
+                return "TOGGLE".startsWith(secondInput.toUpperCase(Locale.ROOT))
+                        ? List.of("toggle")
+                        : Collections.emptyList();
+            }
+
+            if (firstInputUpperCase.equals("VIEW_DISTANCE") || firstInputUpperCase.equals("SIMULATION_DISTANCE")) {
+                return IntStream.rangeClosed(2, 32)
+                        .mapToObj(i -> String.format("%02d", i))
+                        .filter(s -> s.startsWith(secondInput))
+                        .toList();
+            }
+        }
+
+        return Collections.emptyList();
     }
 
     private void sendUsage(CommandSender sender, String label) {
