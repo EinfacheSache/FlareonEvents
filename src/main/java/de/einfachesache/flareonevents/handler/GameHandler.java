db8f455813799b7d040dfe2d5d8f1b9f685b26a5
einfachesache <klugetommy@web.de>
2025-10-04 20:07:19 +0200

    Changes -> changed no pvp time to 15m

--- src/main/java/de/einfachesache/flareonevents/handler/GameHandler.java
+++ src/main/java/de/einfachesache/flareonevents/handler/GameHandler.java
@@ -33,7 +33,7 @@ public class GameHandler {
 
     static final int preparingTime = 3 * 60; // DEFAULT: 3 * 60
     static final int startingTime = 2 * 60; // DEFAULT: 2 * 60
-    static final int noPvPTime = 10 * 60; // DEFAULT: 10 * 60
+    static final int noPvPTime = 15 * 60; // DEFAULT: 15 * 60
     static final int netherOpenTime = 45 * 60; // DEFAULT: 45 * 60
 
     public record BorderPhase(Duration at, int targetSize, Duration shrink) {
